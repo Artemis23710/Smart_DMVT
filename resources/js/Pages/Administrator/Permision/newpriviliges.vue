336489deb09b8bba48eaf5116f905bb3c51d2201
Janith Rangika <83299463+Artemis23710@users.noreply.github.com>
2024-08-09 18:57:53 +0530

    complete admin function 08-09

--- resources/js/Pages/Administrator/Permision/newpriviliges.vue
+++ resources/js/Pages/Administrator/Permision/newpriviliges.vue
@@ -16,63 +16,62 @@ import axios from 'axios';
     <AuthenticatedLayout>
       <div class="row">
         <div class="col-12">
+
           <div class="card">
             <div class="card-body">
-              
-                <form @submit.prevent="submitForm">
-                    <input type="hidden" name="requestid" :value="requestid">
-                    <input type="hidden" name="role_name" :value="role.name">
-
-                    <div class="row col-4">
-                    <div class="form-group">
-                        <InputLabel class="inputlabel">Role Name:</InputLabel>
-                        <input type="text" :value="role.name" id="roleName" class="mt-1 block w-full" disabled />
-                    </div>
-                    </div>
-                    <br>
-                    <label>
-                    <Checkbox
-                        name="selectAll"
-                        id="selectAllDomainList"
-                        v-model="selectAll"
-                        @change="toggleAllPermissions"
-                    />&nbsp;
-                    <label class="form-check-label" style="color: black; margin-top:5px;" for="selectAllDomainList">Check All</label>
-                    </label>
-                    <br><br>
-
-                    <div class="material-datatables">
-                    <table id="datatables" class="table table-striped table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
-                        <tbody>
-                        <tr v-for="module in permsWithModules" :key="module.module_name">
-                            <td><b>{{ module.module_name }}</b></td>
-                            <td v-for="permission in permissions.filter(p => p.module_name === module.module_name)" :key="permission.id">
-                            <Checkbox
-                                class="form-check-input"
-                                style="border-color: black;"
-                                :name="'permissions[]'"
-                                :value="permission.id"
-                                v-model="selectedPermissions"
-                            /> &nbsp;
-                            <label style="color: black; margin-top: 5px;"><b>{{ permission.name }}</b></label>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
-                    </div>
-
-                    <div class="col-md-12 text-center">
-                    <button type="submit" class="btn btn-info"><i class="fas fa-save"></i>&nbsp; Submit</button>
-                    </div>
-                </form>
-
-
+              <form @submit.prevent="submitForm">
+                <input type="hidden" name="requestid" :value="requestid">
+                <input type="hidden" name="role_name" :value="role.name">
+  
+                <div class="row col-4">
+                  <div class="form-group">
+                    <InputLabel class="inputlabel">Role Name:</InputLabel>
+                    <input type="text" :value="role.name" id="roleName" class="mt-1 block w-full" disabled />
+                  </div>
+                </div>
+                <br>
+                <label>
+                  <Checkbox
+                    name="selectAll"
+                    id="selectAllDomainList"
+                    v-model="selectAll"
+                    @click="toggleAllPermissions"
+                  />&nbsp;
+                  <label class="form-check-label" style="color: black; margin-top:5px;" for="selectAllDomainList">Check All</label>
+                </label>
+                <br><br>
+  
+                <div class="material-datatables">
+                  <table id="datatables" class="table table-striped table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
+                    <tbody>
+                      <tr v-for="module in permsWithModules" :key="module.module_name">
+                        <td><b>{{ module.module_name }}</b></td>
+                        <td v-for="permission in permissions.filter(p => p.module_name === module.module_name)" :key="permission.id">
+                          <Checkbox
+                            class="form-check-input"
+                            style="border-color: black;"
+                            :name="'permissions[]'"
+                            :value="permission.id"
+                            v-model="selectedPermissions"
+                          /> &nbsp;
+                          <label style="color: black; margin-top: 5px;"><b>{{ permission.name }}</b></label>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+  
+                <div class="col-md-12 text-center">
+                  <button type="submit" class="btn btn-info"><i class="fas fa-save"></i>&nbsp; Submit</button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
     </AuthenticatedLayout>
   </template>
+  
   
   
   <script>
@@ -99,7 +98,7 @@ import axios from 'axios';
       }
     },
     async submitForm() {
-      try {
+        try {
         await this.$inertia.post('/saveprivilegies', {
           requestid: this.requestid,
           role_name: this.role.name,
@@ -111,8 +110,10 @@ import axios from 'axios';
     }
   }
 };
+
   </script>
-  
+
+
   <style scoped>
   /* Add any necessary custom styles here */
   </style>
